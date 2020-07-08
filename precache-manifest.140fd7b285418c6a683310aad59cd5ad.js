e79db6918d09604d5326f4aed3c3449d637649a6
bakarytoure <bakarytoure57@gmail.com>
2020-07-08 22:49:21 +0000

    Updates

---
+++ precache-manifest.140fd7b285418c6a683310aad59cd5ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a5ea4b3cf22f94c0b6785ad388925e",
+    "url": "/free-deploy/index.html"
+  },
+  {
+    "revision": "cf995276bd7e5e5c45bb",
+    "url": "/free-deploy/static/css/main.1b8d936a.chunk.css"
+  },
+  {
+    "revision": "fc768751d12609719c6d",
+    "url": "/free-deploy/static/js/2.cf794a4d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/free-deploy/static/js/2.cf794a4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf995276bd7e5e5c45bb",
+    "url": "/free-deploy/static/js/main.4f659699.chunk.js"
+  },
+  {
+    "revision": "f7d060d06597e04d2915",
+    "url": "/free-deploy/static/js/runtime-main.7ff2b308.js"
+  }
+]);
